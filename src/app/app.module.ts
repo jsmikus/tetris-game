3ff8e3d964d38fa005c3b5fb732173432e477eb6
jsmikus <jsmikus@gmail.com>
2018-06-26 14:37:46 +0200

    add nav component with with angular modules for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
-import { SharedModule } from "./shared/shared.module";
+import { SharedModule } from './shared/shared.module';
+import { NavComponent } from './nav/nav.component';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     AppRoutingModule,
     SharedModule,
-    HomeModule
+    HomeModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [
