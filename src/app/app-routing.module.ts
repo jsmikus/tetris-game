f957bc4b4f6babbee6c97bea3621a40818df88a6
jsmikus <jsmikus@gmail.com>
2018-06-28 21:57:30 +0200

    add game, rank & about component with routes. modify nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RankComponent } from 'src/app/rank/rank.component';
+import { AboutComponent } from 'src/app/about/about.component';
+import { GameComponent } from 'src/app/game/game.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'app/home', pathMatch: 'full' },
-  { path: 'app', redirectTo: 'app/home', pathMatch: 'full' }
+  { path: 'app', redirectTo: 'app/home', pathMatch: 'full' },
+  { path: 'game', component: GameComponent, data: { title: "Let's play!" } },
+  { path: 'rank', component: RankComponent, data: { title: "Best Scores" } },
+  { path: 'about', component: AboutComponent, data: { title: "How it was created?" } }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { NavComponent } from './nav/nav.component';
+import { RankComponent } from './rank/rank.component';
+import { AboutComponent } from './about/about.component';
 
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    RankComponent,
+    AboutComponent,
+    GameComponent
   ],
   imports: [
     AppRoutingModule,
