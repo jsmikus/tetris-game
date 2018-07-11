f506d4b5f7095bc83aae2784512bb0eb7acd9fd1
jsmikus <jsmikus@gmail.com>
2018-07-11 15:36:46 +0200

    add the game window

--- src/app/modules/game/game.component.ts
+++ src/app/modules/game/game.component.ts
@@ -10,37 +10,14 @@ export class GameComponent {
   title = 'app';
 
   constructor() {
-    // The application will create a renderer using WebGL, if possible,
-    // with a fallback to a canvas render. It will also setup the ticker
-    // and the root stage PIXI.Container
-    const app = new PIXI.Application();
+    // Create a Pixi Application
+    const app = new PIXI.Application({width: 600, height: 800});
 
-    // The application will create a canvas element for you that you
-    // can then insert into the DOM
+    app.renderer.view.style.display = 'block';
+    app.renderer.view.style.margin = 'auto';
+
+    // Add the canvas that Pixi automatically created for you to the HTML document
     document.body.appendChild(app.view);
-
-    // load the texture we need
-    PIXI.loader.add('bunny', 'https://pixijs.io/examples/required/assets/basics/bunny.png').load((loader, resources) => {
-      // This creates a texture from a 'bunny.png' image
-      const bunny = new PIXI.Sprite(resources.bunny.texture);
-
-      // Setup the position of the bunny
-      bunny.x = app.renderer.width / 2;
-      bunny.y = app.renderer.height / 2;
-
-      // Rotate around the center
-      bunny.anchor.x = 0.5;
-      bunny.anchor.y = 0.5;
-
-      // Add the bunny to the scene we are building
-      app.stage.addChild(bunny);
-
-      // Listen for frame updates
-      app.ticker.add(() => {
-        // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-      });
-    });
   }
 
 }
