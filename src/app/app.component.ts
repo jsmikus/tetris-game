d0157775a5c448606ff32895f4e9f0a1890a9841
jsmikus <jsmikus@gmail.com>
2018-07-17 11:16:27 +0200

    add fade effect animation for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [fadeAnimation] // register the animation
 })
 export class AppComponent {
 }
